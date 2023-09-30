8c247f09d0fba856e5c02cab09046f53b8167b2a
pc <a@email>
2023-09-30 11:19:02 +0500

    1

--- bin/myapp.dart
+++ bin/myapp.dart
@@ -1,13 +1,20 @@
-import 'package:dio/dio.dart';
-
-import 'models/product/product.dart';
-import 'models/resp/resp.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert' as convert;
 
 void main(List<String> arguments) async {
-  var clientHttp = Dio();
-  var response = await clientHttp.get('https://dummyjson.com/products');
+  final response = await http.get(Uri.https('dummyjson.com', '/products'));
+  Map<String, dynamic> data = convert.jsonDecode(response.body);
 
-  var respData = resp.fromJson(response.data);
+  List<dynamic> products = data['products'];
+  List<dynamic> ratings = products.map((r) => r['rating'] as dynamic).toList();
 
-  print(respData.products);
+  double sum = 0.0;
+
+  ratings.forEach(
+    (element) {
+      sum += element;
+    },
+  );
+  print(sum / ratings.length);
+  print(sum);
 }
